be7ae1104d7c490c386c92c265f626206e4f372b
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-05-25 19:42:45 +0600

    review section img added

--- src/Component/pages/Home/Review.jsx
+++ src/Component/pages/Home/Review.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 
-const Review = ({review}) => {
-    const {name,img,rating,describe} = review
-    let ratings = []
-    for(let i = 0;i < rating; i ++){
-        ratings.push(<i  className="fa-solid fa-star text-yellow-600"></i>)
-    }
+const Review = ({ review }) => {
+  const { name, img, rating, describe } = review;
+  let ratings = [];
+  for (let i = 0; i < rating; i++) {
+    ratings.push(<i className="fa-solid fa-star text-yellow-600"></i>);
+  }
   return (
-    <div  className="card bg-base-100 shadow-xl">
-      <div  className="card-body">
-        <div  className="card-actions justify-center flex-col items-center">
-            <img src={img} alt="" className="w-20"/>
-            <h3 className="text-xl font-semibold">{name}</h3>
-            <p>{describe}</p>
-            <div>{ratings.map((star,index) => <span key={index}>{star}</span>)}</div>
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <div className="card-actions justify-center flex-col items-center">
+          {img ? <div class="avatar">
+            <div class="w-24 rounded-full">
+              <img src={img} alt={name + "feedback"} />
+            </div>
+          </div>:
+          <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+              <span class="text-3xl">{name.slice(0,1)}</span>
+            </div>
+          </div>}
+          <h3 className="text-xl font-semibold">{name}</h3>
+          <p>{describe}</p>
+          <div>
+            {ratings.map((star, index) => (
+              <span key={index}>{star}</span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
