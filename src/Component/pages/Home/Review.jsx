9e26e305b7f1a61feb620c51d982c49d3d006b05
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-06-24 17:11:21 +0600

    review.jsx  section bug fixed

--- src/Component/pages/Home/Review.jsx
+++ src/Component/pages/Home/Review.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Review = ({ review }) => {
   const { name, img, rating, describe } = review;
   let ratings = [];
@@ -10,16 +8,21 @@ const Review = ({ review }) => {
     <div data-aos="zoom-in" className="card bg-base-100 shadow-xl">
       <div className="card-body">
         <div className="card-actions justify-center flex-col items-center">
-          {img ? <div  className="avatar">
-            <div  className="w-24 rounded-full">
-              <img src={img} alt={name + "feedback"} />
+          {img ? (
+            <div className="avatar">
+              <div className="w-24 rounded-full">
+                <img src={img} alt={name + "feedback"} />
+              </div>
             </div>
-          </div>:
-          <div  className="avatar placeholder">
-            <div  className="bg-neutral-focus text-neutral-content rounded-full w-24">
-              <span  className="text-3xl">{name.slice(0,1).toUpperCase()}</span>
+          ) : (
+            <div className="avatar placeholder">
+              <div className="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span className="text-3xl">
+                  {name.slice(0, 1).toUpperCase()}
+                </span>
+              </div>
             </div>
-          </div>}
+          )}
           <h3 className="text-xl font-semibold">{name}</h3>
           <p>{describe}</p>
           <div>
--- src/Component/pages/Reviews/Reviews.jsx
+++ src/Component/pages/Reviews/Reviews.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useQuery } from "react-query";
 import axiosPrivate from "../../../axiosPrivate/axiosPrivate";
 import Footer from "../../Shared/Footer/Footer";
@@ -7,26 +6,26 @@ import PageTitle from "../../Shared/PageTitle/PageTitle";
 import Review from "../Home/Review";
 
 const Reviews = () => {
-    const {data: reviews,isLoading} = useQuery('reviews',() => {
-        return axiosPrivate.get(`${import.meta.env.VITE_SERVER_URL}/review`)
-    })
-    if(isLoading){
-        return <Loading className='text-black'></Loading>
-    }
+  const { data: reviews, isLoading } = useQuery("reviews", () => {
+    return axiosPrivate.get(`${import.meta.env.VITE_SERVER_URL}/review`);
+  });
+  if (isLoading) {
+    return <Loading className="text-black"></Loading>;
+  }
   return (
     <>
-    <div className="sm:container sm:mx-auto mx-5">
-      <PageTitle title='Reviews'></PageTitle>
-      <h1 className="text-4xl text-stone-700 font-semibold">
-        Customer Reviews
-      </h1>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 my-5">
-        {
-            reviews.data.map(review => <Review key={review._id} review={review}></Review>)
-        }
+      <div className="sm:container sm:mx-auto mx-5">
+        <PageTitle title="Reviews"></PageTitle>
+        <h1 className="text-4xl text-stone-700 font-semibold">
+          Customer Reviews
+        </h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 my-5">
+          {reviews?.data?.map((review) => (
+            <Review key={review._id} review={review}></Review>
+          ))}
+        </div>
       </div>
-    </div>
-    <Footer></Footer>
+      <Footer></Footer>
     </>
   );
 };
