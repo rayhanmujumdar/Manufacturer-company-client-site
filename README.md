0bdd2a54cf1cd9430e82cd893ce1ba03ba358797
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2024-05-19 13:40:32 +0600

    readme.md file updated

--- README.md
+++ README.md
@@ -1,19 +1,47 @@
+**Computer Market ( Manufacture Website )**
+
+Use Technologies: JavaScript(ES6), React.js, Tailwind CSS, Firebase, Node.js, Express.js,
+
+MongoDB(Database), Stripe, JWT(JSON web token)
+
+- Implement a beautiful user interface with React.js
+- Full-stack Website using the React, Node.js, Express.js, MongoDB, React
+
+query, React Form, React Modal, Tailwind CSS, JWT, etc
+
+- Implemented a Firebase authentication system and JWT-based
+
+authorization.
+
+- Mobile, tablet, desktop, and notepad responsive website.
+- Dynamic Stripe Payment Gateway system
+- Applied a dashboard for super admin who can add manage services, Make
+
+another admin with an Email address.
+
+- Admin Email: rayhanmojumdar0177@gmail.com
+- Password: 121212
+- Github client-side repo: [url](https://github.com/rayhanmujumdar/Manufacturer-company-client-site.git)
+- Github Server-side repo: [url](https://github.com/rayhanmujumdar/Manufacturer-company-server-site)
+- Project-live-url: [url](https://assignment-12-full-stack.web.app/)
 # What technology i am using in this project?
---------------------------------------------------
-1.tailwind --
-2.react router dom --
-3.react form hook --
-4.react firebase hook--
-5.daiyusi ui --
-6.react query --
-7.stripe (prement)--
-8.toast --
-9.tailwind element --
-10.firebase -- 
-11.axios --
-12.react modal --
-13.react-helmet-async --
-14.aos animation --
+---
 
+-   1.tailwind
+-   2.react router dom
+-   3.react form hook
+-   4.react firebase hook
+-   5.daiyusi ui
+-   keep react
+-   6.react query
+-   7.stripe (payment gateway)
+-   8.react hot toast
+-   9.tailwind css
+-   10.firebase
+-   11.axios
+-   12.react modal
+-   13.react-helmet-async
+-   14.aos animation
 
-computer Market Live site link [live link](https://assignment-12-full-stack.web.app/)
+computer Market Live site link
+[live link](https://assignment-12-full-stack.web.app/)
--- src/Component/pages/Login/ForgetPassword.jsx
+++ src/Component/pages/Login/ForgetPassword.jsx
@@ -1,55 +1,64 @@
-import React, { useEffect, useState } from "react";
-import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
-import auth from "../../../firebase/firebase.init";
-import toast from "react-hot-toast";
+import { useEffect, useState } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase/firebase.init';
 
 export default function ForgetPassword() {
-  const [sendPasswordResetEmail, sending, resetError] =
-    useSendPasswordResetEmail(auth);
-  const [email, setEmail] = useState("");
-  useEffect(() => {
-    if (resetError) {
-      toast.error(resetError.message, {
-        id: "error",
-      });
-    }
-  }, [resetError]);
-  const handleResetPassword = async (e) => {
-    e.preventDefault();
-    await sendPasswordResetEmail(email);
-    if (resetError) {
-      toast.error("Sent not email", {
-        id: "error",
-      });
-    } else {
-      toast.success("Sent email", {
-        id: "success",
-      });
-    }
-  };
-  return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <div className="join">
-            <h1 className="text-2xl font-bold pb-3">Reset password mail</h1>
-            <form
-              onSubmit={handleResetPassword}
-              className="flex justify-center items-center"
-            >
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                className="input join-item w-96"
-                placeholder="Email"
-              />
-              <button type="submit" className="btn join-item rounded-r-full">
-                Send
-              </button>
-            </form>
-          </div>
+    const [sendPasswordResetEmail, sending, resetError] =
+        useSendPasswordResetEmail(auth);
+    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (resetError) {
+            toast.error(resetError.message, {
+                id: 'error',
+            });
+        }
+    }, [resetError]);
+    const handleResetPassword = async e => {
+        e.preventDefault();
+        await sendPasswordResetEmail(email);
+        if (resetError) {
+            toast.error('Sent not email', {
+                id: 'error',
+            });
+        } else {
+            toast.success('Sent email', {
+                id: 'success',
+            });
+            navigate('/login', { replace: true });
+        }
+    };
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <div className="join">
+                        <h1 className="text-2xl font-bold pb-3">
+                            Reset password mail
+                        </h1>
+                        <form
+                            onSubmit={handleResetPassword}
+                            className="flex justify-center items-center"
+                        >
+                            <input
+                                onChange={e => setEmail(e.target.value)}
+                                value={email}
+                                className="input join-item w-96"
+                                placeholder="Email"
+                            />
+                            <button
+                                disabled={sending}
+                                type="submit"
+                                className="btn join-item rounded-r-full"
+                            >
+                                Send
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
